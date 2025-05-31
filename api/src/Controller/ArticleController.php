1140425583837656b3fda66e28615f09c268b9a8
J-BT <jeanbaptiste.theroulde@gmail.com>
2025-05-31 15:48:17 +0200

    CN-4 : /api/articles endpoint works

--- api/src/Controller/ArticleController.php
+++ api/src/Controller/ArticleController.php
@@ -16,29 +16,25 @@ use App\Service\RssService;
 class ArticleController extends AbstractController
 {
     # Attributes & Accessors
-    // private RssService
-    # Constructors
+    private RssService $rssService;
 
+    # Constructors
+    public function __construct(RssService $rssService){
+        $this->rssService = $rssService;
+    }
+    
     # Methods
 
     #[Route('/api/articles', name: 'app_article_get', methods:['GET'])]
     public function getAll(): Response
     {
         try {
-            return $this->json([
-                'message' => '日本語が読めるかな？　出来たら、APIの開発が開発し始める。',
-                'path' => 'src/Controller/ArticleController.php',
-            ], Response::HTTP_OK, [
+            $articles = $this->rssService->fetchAllArticles();
+
+            return $this->json($articles, Response::HTTP_OK, [
                 'Content-Type' => 'application/json; charset=utf-8'
             ]);
-            // $json = json_encode([
-            //     'message' => 'OK OK',
-            //     'path' => 'src/Controller/ArticleController.php',
-            // ]);
 
-            // return new Response($json, Response::HTTP_OK, [
-            //     'content-type' => 'application/json'
-            // ]);
         } catch (Throwable $e) {
             return $this->json([
             'errorType' => get_class($e),
