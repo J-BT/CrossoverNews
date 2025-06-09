af6974df33a7721d604ff7d3c7610fd09b0d1d1e
J-BT <jeanbaptiste.theroulde@gmail.com>
2025-06-09 18:07:20 +0200

    CN-13 : UI ready for docker tests

--- app/src/components/pages/Home.jsx
+++ app/src/components/pages/Home.jsx
@@ -1,40 +1,5 @@
 
-// import './Home.scss'
-// import Header from '../tools/header/Header';
-// import Card from '../tools/article/Card';
-// import Footer from '../tools/footer/Footer';
-// import exampleArticles from '../../../src/if-no-backend/example-articles-too-big.json';
-
-// function Home() {
-
-//     const articles = exampleArticles.flatMap(item =>
-//         item.content.map(article => ({
-//             zone: item.zone,
-//             source: item.source,
-//             title: article.title,
-//             date: article.date.replace(/\s\+\d{4}$/, ''),
-//             link: article.link
-//         }))
-//     );
-//     return (
-//         <div className="app">
-//             <Header />
-//             <main className="main-content">
-//                 <section className="news-cards">
-//                     {articles.map((article, index) => (
-//                         <Card key={index} {...article} />
-//                     ))}
-
-//                 </section>
-//             </main>
-//             <Footer articles={articles} />
-//         </div>
-//     );
-// }
-
-// export default Home;
-
-
+import { useEffect, useState } from 'react';
 import './Home.scss'
 import Header from '../tools/header/Header';
 import Card from '../tools/article/Card';
@@ -42,29 +7,28 @@ import Footer from '../tools/footer/Footer';
 import exampleArticles from '../../../src/if-no-backend/example-articles-too-big.json';
 
 function Home() {
-    const groupedArticles = exampleArticles.map(item => ({
-        zone: item.zone,
-        source: item.source,
-        articles: item.content.map(article => ({
-            title: article.title,
-            date: article.date.replace(/\s\+\d{4}$/, ''),
-            link: article.link
-        }))
-    }));
+    const [articles, setArticles] = useState([]);
+    const [groupedArticles, setGroupedArticles] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-    const articles = exampleArticles.flatMap(item =>
-        item.content.map(article => ({
-            zone: item.zone,
-            source: item.source,
-            title: article.title,
-            date: article.date.replace(/\s\+\d{4}$/, ''),
-            link: article.link
-        }))
-    );
+    useEffect(() => {
+
+        FetchArticles('http://127.0.0.1:11111/api/articles', exampleArticles,
+            setArticles, setGroupedArticles, setIsLoading, setError);
+
+    }, []);
+
+    if (isLoading) return <div>Chargement...</div>;
+    if (error) return <div>Erreur : {error.message}</div>;
+
 
     return (
         <div className="app">
             <Header />
+            <strong>
+                {!__USE_API__ ? '__USE_API__ inactif — JSON local' : ''}
+            </strong>
             <main className="main-content">
                 <section className="news-cards">
                     {groupedArticles.map((group, index) => (
@@ -78,3 +42,63 @@ function Home() {
 }
 
 export default Home;
+
+
+/************************* Private Methods**************************/
+/*******************************************************************/
+
+function FetchArticles(apiUrl, exampleData,
+    setArticles, setGroupedArticles, setIsLoading, setError
+) {
+    if (__USE_API__) {
+        fetch(apiUrl)
+            .then((res) => {
+                if (!res.ok) throw new Error('Erreur API');
+                return res.json();
+            })
+            .then((data) => {
+
+                let articles = GetArticlesDetails(data);
+                setGroupedArticles(articles.grouped);
+                setArticles(articles.flat);
+                setIsLoading(false);
+            })
+            .catch((err) => {
+                console.error('Erreur lors du fetch:', err);
+                setError(err);
+                setIsLoading(false);
+            });
+    } else {
+
+        let articles = GetArticlesDetails(exampleData);
+        setGroupedArticles(articles.grouped);
+        setArticles(articles.flat);
+        setIsLoading(false);
+    }
+}
+
+
+function GetArticlesDetails(articles) {
+    const grouped = articles.map(item => ({
+        zone: item.zone,
+        source: item.source,
+        articles: item.content.map(article => ({
+            title: article.title,
+            date: article.date.replace(/\s\+\d{4}$/, ''),
+            link: article.link
+        }))
+    }));
+
+    const flat = articles.flatMap(item =>
+        item.content.map(article => ({
+            zone: item.zone,
+            source: item.source,
+            title: article.title,
+            date: article.date.replace(/\s\+\d{4}$/, ''),
+            link: article.link
+        }))
+    );
+
+    return { 'grouped': grouped, 'flat': flat }
+
+}
